6d9f21b08d4af6cb7b4c27c061998cc8a4584913
Leonardo Agrio <leo13nardo84@gmail.com>
2024-05-20 09:00:44 -0500

    main chica

--- OverWorldMap.js
+++ OverWorldMap.js
@@ -26,12 +26,13 @@ window.OverworldMaps = {
         upperSrc: "/images/maps/KitchenUpper.png",
         gameObjects: {
             girl: new Person({
+                isPlayerControlled: true,
                 x: utils.withGrid(4),
                 y: utils.withGrid(6),
                 src: "images/people/girlPokemon.png"
             }),
             hero : new Person({
-                isPlayerControlled: true,
+                isPlayerControlled: false,
                 x:utils.withGrid(5),
                 y:utils.withGrid(4),
                 src:"images/people/redMod.png"
